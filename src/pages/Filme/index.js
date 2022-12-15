3ae912718784f882f948e3ebebfe544e7241649b
user <vitorfds18@gmail.com>
2022-12-15 14:09:40 -0300

    commit inicial

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,13 @@
+
+function Filme(){
+    return(
+
+            <div>
+                    <h1>BEM VINDO A PAGINA DO FILME</h1>
+
+            </div>
+
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+
+function Home(){
+    return(
+
+    <div>
+        <h1>bem vindo a Home</h1>
+    </div>
+    
+    );
+}
+
+export default Home;
