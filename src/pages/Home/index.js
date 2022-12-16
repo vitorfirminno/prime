aaa4d79466ad98ad3b9f886cc82f28963083a3dd
user <vitorfds18@gmail.com>
2022-12-15 21:51:19 -0300

    implementação da api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react';
+import api from '../../services/api'
+
+// URL DA API: /movie/now_playing?api_key=e1802bbd13738ac082fe5174142460e3
+
 
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+
+
+    useEffect(() =>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing" , {params:{
+                api_key:"e1802bbd13738ac082fe5174142460e3",
+                language: "pt-BR",
+                page: 1 ,
+            }
+        });
+
+        console.log(response.data.result);
+        }
+    })
+
+    
+
     return(
 
     <div>
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import { getDefaultNormalizer } from '@testing-library/react';
+import axios from 'axios';
+
+//BASE DA URL: https://api.themoviedb.org/3
+// URL DA API: /movie/now_playing?api_key=e1802bbd13738ac082fe5174142460e3
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3'
+});
+
+export default api;
+
+
+
