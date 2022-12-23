ea42bece01954678d3bd56e27a1e7784b9488546
user <vitorfds18@gmail.com>
2022-12-23 12:09:25 -0300

    Implementação de telas e loading

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,64 @@
-import { useEffect, useState } from 'react';
-import api from '../../services/api'
+import { useEffect, useState} from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
-// URL DA API: /movie/now_playing?api_key=e1802bbd13738ac082fe5174142460e3
-
+// URL DA API: /movie/now_playing?api_key=28fc232cc001c31e8a031f419d0a14ca&language=pt-BR
 
 function Home(){
-    const [filmes, setFilmes] = useState([]);
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] =useState(true);
 
 
-    useEffect(() =>{
-        async function loadFilmes(){
-            const response = await api.get("movie/now_playing" , {params:{
-                api_key:"e1802bbd13738ac082fe5174142460e3",
-                language: "pt-BR",
-                page: 1 ,
-            }
-        });
+  useEffect(()=>{
 
-        console.log(response.data.result);
+    async function loadFilmes(){
+      const response = await api.get("movie/now_playing", {
+        params:{
+         api_key: "28fc232cc001c31e8a031f419d0a14ca",
+         language: "pt-BR",
+         page: 1,
         }
-    })
+      })
 
-    
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10)); //numero de itens
+      setLoading(false);
 
+    }
+
+    loadFilmes();
+
+  }, [])
+
+  if(loading){
     return(
+      <div className='loading'>
+        <h2>
+          Carregando filmes...
+        </h2>
+        <img/>       
 
-    <div>
-        <h1>bem vindo a Home</h1>
+      </div>
+    )
+
+  }
+
+  return(
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
-    
-    );
+  )
 }
 
 export default Home;
